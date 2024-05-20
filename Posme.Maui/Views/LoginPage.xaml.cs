5c31cb398a534a184c7bf1889dcfadc222e78705
Witman Gonzalez <gwitman>
2024-05-19 18:06:48 -0600

    revision

--- Posme.Maui/Views/LoginPage.xaml.cs
+++ Posme.Maui/Views/LoginPage.xaml.cs
@@ -9,22 +9,25 @@ namespace Posme.Maui.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        private readonly IServiceProvider _services;
+        private readonly IRepositoryTbUser? _tbUserRespository;
         private readonly RestApiCoreAcount _restServiceUser = new();
-        private readonly IRepositoryTbUser _tbUserRespository;
 
-        public LoginPage()
+        public LoginPage(IServiceProvider services)
         {
+            _services = services;
             InitializeComponent();
             BindingContext = new LoginViewModel();
+            _tbUserRespository = services.GetService<IRepositoryTbUser>();
         }
 
         protected override async void OnAppearing()
         {
-            var findUserRemember = await _tbUserRespository.PosmeFindUserRemember();
+            var findUserRemember = await _tbUserRespository!.PosmeFindUserRemember();
             if (findUserRemember is null) return;
-            ((LoginViewModel)BindingContext).UserName = findUserRemember.Nickname;
-            ((LoginViewModel)BindingContext).Password = findUserRemember.Password;
-            ((LoginViewModel)BindingContext).Company = findUserRemember.Company;
+            ((LoginViewModel)BindingContext).UserName = findUserRemember.Nickname!;
+            ((LoginViewModel)BindingContext).Password = findUserRemember.Password!;
+            ((LoginViewModel)BindingContext).Company = findUserRemember.Company!;
         }
 
         private async void DXButtonBase_OnClicked(object sender, EventArgs e)
@@ -33,7 +36,7 @@ namespace Posme.Maui.Views
             var model = (LoginViewModel)BindingContext;
             VariablesGlobales.CompanyKey = model.Company.ToLower();
             var findUserRemember =
-                await _tbUserRespository.PosMeFindUserByNicknameAndPassword(model.UserName, model.Password);
+                 await _tbUserRespository!.PosMeFindUserByNicknameAndPassword(model.UserName, model.Password);
             if (ChkRemember.IsChecked!.Value)
             {
                 model.PopupShow = await _restServiceUser.LoginMobile(model.UserName, model.Password);
@@ -55,14 +58,14 @@ namespace Posme.Maui.Views
                 }
                 else
                 {
-                    VariablesGlobales.User.Company = model.Company;
+                    VariablesGlobales.User!.Company = model.Company;
                     VariablesGlobales.User.Remember = true;
                     await _tbUserRespository.PosMeInsert(VariablesGlobales.User);
                 }
             }
             else
             {
-                if (await _tbUserRespository.PosMeRowCount()<=0)
+                if (await _tbUserRespository.PosMeRowCount() <= 0)
                 {
                     model.Mensaje = Mensajes.MensajeSinDatosTabla;
                     model.MensajeCommand.Execute(null);
@@ -81,7 +84,7 @@ namespace Posme.Maui.Views
             }
 
             await Navigation.PopModalAsync();
-            Application.Current!.MainPage = new MainPage();
+            Application.Current!.MainPage = new MainPage(_services);
         }
 
         private void ClosePopup_Clicked(object sender, EventArgs e)
