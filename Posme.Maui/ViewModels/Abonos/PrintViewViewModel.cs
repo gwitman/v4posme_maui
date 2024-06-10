de731a9237f9583a703fe13b1f76b9a1f929eee5
jose miguel romero sevilla <jmiguelsevilla@hotmail.com>
2024-06-10 14:35:01 -0600

    no se subieron estos archivos

---
+++ Posme.Maui/ViewModels/Abonos/PrintViewViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Posme.Maui.Services.Helpers;
+
+namespace Posme.Maui.ViewModels.Abonos;
+
+public class PrintViewViewModel :BaseViewModel
+{
+    public PrintViewViewModel()
+    {
+        
+    }
+
+    public void OnAppearing()
+    {
+        DocumentStream = new FileStream(VariablesGlobales.FilePdf, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+    }
+    
+    private Stream? _documentStrem;
+
+    public Stream? DocumentStream
+    {
+        get => _documentStrem;
+        set => SetProperty(ref _documentStrem, value);
+    }
+}
---
+++ Posme.Maui/Views/Abonos/PrintViewPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Posme.Maui.ViewModels.Abonos;
+
+namespace Posme.Maui.Views.Abonos;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class PrintViewPage : ContentPage
+{
+    public PrintViewPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        ((PrintViewViewModel)BindingContext).OnAppearing();
+    }
+}
