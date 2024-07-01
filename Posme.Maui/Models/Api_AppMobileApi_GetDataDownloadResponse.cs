bb24790a9e68e36616b59c6d15752dd1a1ae3123
Witman Gonzalez <gwitman>
2024-07-01 08:29:25 -0600

    revision

--- Posme.Maui/Models/Api_AppMobileApi_GetDataDownloadResponse.cs
+++ Posme.Maui/Models/Api_AppMobileApi_GetDataDownloadResponse.cs
@@ -44,6 +44,8 @@ public class Api_AppMobileApi_GetDataDownloadCustomerResponse : BindableBase
 
     [DataMember] public int CurrencyId { get; set; }
 
+    [DataMember] public string? CurrencyName { get; set; }
+
     [DataMember] public int CustomerCreditLineId { get; set; }
     public bool Modificado { get; set; }
 
